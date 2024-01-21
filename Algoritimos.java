32304a8b9f6a465810dbfed025c2b2b3dbf32cf2
HevertonMac <hevertonmacedo23@gmail.com>
2024-01-20 21:48:20 -0300

    eight

--- Algoritimos.java
+++ Algoritimos.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Stack;
 
-public class Algoritimos {
+public class Algoritimos{
 
     public int AlgoritmoDijkstra(Grafo grafo, Vertice inicio, Vertice destino) {
         Map<Vertice, Integer> distanciaMinima = new HashMap<>();
@@ -31,5 +34,62 @@ public class Algoritimos {
 
         return distanciaMinima.get(destino);
     }
-    
+
+    public List<Vertice> AlgoritmoHierholzer(Grafo grafo) {
+        List<Vertice> cicloEuleriano = new ArrayList<>();
+
+        // Verifica se o grafo possui um ciclo euleriano
+        if (!verificarCicloEuleriano(grafo)) {
+            System.out.println("O grafo não possui um ciclo euleriano.");
+            return cicloEuleriano;
+        }
+
+        // Encontra um vértice com grau ímpar (se existir) ou escolhe o primeiro vértice
+        Vertice inicio = encontrarVerticeInicio(grafo);
+
+        // Executa o algoritmo de Hierholzer
+        Stack<Vertice> pilha = new Stack<>();
+        pilha.push(inicio);
+
+        while (!pilha.isEmpty()) {
+            Vertice atual = pilha.peek();
+
+            if (atual.getGrau() == 0) {
+                // Se o vértice não tem mais arestas, adiciona ao ciclo
+                cicloEuleriano.add(atual);
+                pilha.pop();
+            } else {
+                // Caso contrário, escolhe uma aresta, remove e adiciona o próximo vértice na pilha
+                Aresta aresta = atual.getArestas().get(0);
+                Vertice proximo = aresta.destino;
+                atual.diminuirGrau();
+                aresta.destino.diminuirGrau();
+                pilha.push(proximo);
+            }
+        }
+
+        return cicloEuleriano;
+    }
+
+    private boolean verificarCicloEuleriano(Grafo grafo) {
+        // Verifica se cada vértice tem um grau par
+        for (Vertice vertice : grafo.getVertices()) {
+            if (vertice.getGrau() % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Vertice encontrarVerticeInicio(Grafo grafo) {
+        // Encontra um vértice com grau ímpar (se existir) ou escolhe o primeiro vértice
+        for (Vertice vertice : grafo.getVertices()) {
+            if (vertice.getGrau() % 2 != 0) {
+                return vertice;
+            }
+        }
+        return grafo.getVertices().get(0);
+    }
+
+
 }
